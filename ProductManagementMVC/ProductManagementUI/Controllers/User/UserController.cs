031dd116ec5cef13af754364c3d1c584743005a5
FOX-旧梦 <FOX-@FOX-JIUMENG>
2020-03-20 11:49:15 +0800

    建mvc控制器  声明页面&方法命名规范

---
+++ ProductManagementMVC/ProductManagementUI/Controllers/User/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductManagementUI.Controllers
+{
+    public class UserController : Controller
+    {
+
+        /*
+         * 页面用 功能名称+Page   例如 UserLoginPage
+         * 方法使用 功能名称+Action   例如 UserLoginAction
+         */
+
+        // GET: User
+        public ActionResult UserLoginPage()
+        {
+            return View();
+        }
+
+        public void UserLoginAction()
+        { 
+
+        }
+    }
+}
