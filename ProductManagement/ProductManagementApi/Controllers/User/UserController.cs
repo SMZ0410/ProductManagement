fd4ebaa93734a236e87ac079c0ec6dd324f2aebb
FOX-旧梦 <FOX-@FOX-JIUMENG>
2020-03-21 16:01:36 +0800

    登录接口完成

--- ProductManagement/ProductManagementApi/Controllers/User/UserController.cs
+++ ProductManagement/ProductManagementApi/Controllers/User/UserController.cs
@@ -7,23 +7,37 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using ProductManagementApi.Auth;
+using SDKClient.Api.Response.User;
+using SDKClient.Api.Request.User;
 
 namespace ProductManagementApi.Controllers.User
 {
     /// <summary>
     /// 用户Api控制器
     /// </summary> 
-    [ApiAuthorize]
-    
+    [ApiAuthorize] 
     public class UserController : ApiController
     {
-        /// 获取用户基本信息
+       
+        /// <summary>
+        /// 获取用户信息
         /// </summary>
-        /// <returns>用户集合信息</returns>
+        /// <returns></returns>
         [HttpPost]
         public UserGetResponse GetUsers()
         {
             return UserBll.Instance.GetUsers();
+        }
+
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public UserLoginResponse UserLogin(UserLoginRequest request)
+        {
+            return UserBll.Instance.UserLogin(request);
         }
     }
 }
