3a07a7c5f50b9c4858807c23865cb80027cb5c65
FOX-旧梦 <FOX-@FOX-JIUMENG>
2020-03-22 20:25:22 +0800

    MVC.BLL 换成单例模式

--- ProductManagement/ProductManagementApi/Controllers/Product/ProductController.cs
+++ ProductManagement/ProductManagementApi/Controllers/Product/ProductController.cs
@@ -10,7 +10,6 @@ using SDKClient.Api.Request;
 using SDKClient.Api.Request.Product;
 using SDKClient.Api.Response.Product;
 
-
 namespace ProductManagementApi.Controllers.Product
 {
     /// <summary>
--- ProductManagementMVC/BLL/Product/ProductBll.cs
+++ ProductManagementMVC/BLL/Product/ProductBll.cs
@@ -1,6 +1,7 @@
 ﻿using BLL.ApiRequest;
 using SDKClient.Api.Request.Product;
 using SDKClient.Api.Response;
+using SDKClient.Api.Response.Product;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace BLL.Product
 {
-    public class ProductBll
+    public class ProductBll:BaseBll<ProductBll>
     { 
 
         /// <summary>
