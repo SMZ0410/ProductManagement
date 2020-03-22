1377ef6d43b76cf8bb677eb6c3c0d9b1f960d7d2
lenovo <lenovo@LAPTOP-MGN7U2GM>
2020-03-22 20:37:26 +0800

    用户显示mvc

---
+++ ProductManagementMVC/BLL/User/UserBll.cs
@@ -0,0 +1,23 @@
+﻿using BLL.ApiRequest;
+using SDKClient.Api.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SDKClient.Api.Request.User;
+
+namespace BLL.User
+{
+    public class UserBll
+    {
+        /// <summary>
+        /// 获取用户信息
+        /// </summary>
+        /// <returns></returns>
+        public UserGetResponse GetUsers(UserGetRequest request)
+        {
+            return ApiRequestHelper.Post<UserGetRequest, UserGetResponse>(request);
+        }
+    }
+}
---
+++ ProductManagementMVC/ProductManagementUI/Models/UserInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductManagementUI.Models
+{
+    /// <summary>
+    /// 用户管理表
+    /// </summary>
+    public class UserInfo
+    {
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int UserId { get; set; }
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 所属地
+        /// </summary>
+        public string AddressName { get; set; }
+        /// <summary>
+        /// 角色
+        /// </summary>
+        public string RoleName { get; set; }
+    }
+}
