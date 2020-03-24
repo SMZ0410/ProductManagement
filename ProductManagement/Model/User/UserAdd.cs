6ee11b77a32331444bec5a2d0735e20227ee7f29
lenovo <lenovo@LAPTOP-MGN7U2GM>
2020-03-24 19:53:43 +0800

    拉取一下

---
+++ ProductManagement/Model/User/UserAdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.User
+{
+    public class UserAdd
+    {
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 所在地
+        /// </summary>
+        public string AddressName { get; set; }
+        /// <summary>
+        /// 关联角色
+        /// </summary>
+        public string RoleName { get; set; }
+    }
+}
