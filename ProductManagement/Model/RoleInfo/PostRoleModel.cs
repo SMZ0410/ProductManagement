db0a923dac789e322b32c46155dc879a7e2de862
zoujiazheng <2411269847@qq.com>
2020-03-27 10:41:57 +0800

    逻辑删除

---
+++ ProductManagement/Model/RoleInfo/PostRoleModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.RoleInfo
+{
+    public class PostRoleModel
+    {
+        public string RoleName { get; set; }
+        public string RoleDescribe { get; set; }
+        public int CreatorId { get; set; }
+        public int UpdatorId { get; set; }
+        public int Status { get; set; }
+        public int RoleId { get; set; }
+        public int PrivilegeId { get; set; }
+        public string privilegeName { get; set; }
+        public string privilegeDescribe { get; set; }
+        public int UserId { get; set; }
+    }
+}
