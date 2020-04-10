79accc452e8254490d78e93f0ee2af4a2ddfb407
FOX-旧梦 <FOX-@FOX-JIUMENG>
2020-04-10 10:25:02 +0800

    该改角色删除的bug

--- ProductManagement/SDKClient/API/Response/Role/RoleUpdateResponse.cs
+++ ProductManagement/SDKClient/API/Response/Role/RoleUpdateResponse.cs
@@ -8,7 +8,7 @@ using Model.RoleInfo;
 namespace SDKClient.Api.Response.Role
 {
     public class RoleUpdateResponse : BaseResponse
-    {
-        public int postRoles { get; set; }
+    { 
+        
     }
 }
